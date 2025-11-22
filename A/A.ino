4d7ff646f322dc1ceada34652d03d34166b4dc8c
kourosh-forti-hands <kourosh@DESKTOP-173JGM1.localdomain>
2025-11-22 13:34:34 -0600

    Update OTA configuration for GitHub Pages hosting (v1.3.0b1)

--- A/A.ino
+++ A/A.ino
@@ -20,6 +20,7 @@ const String VERSION = "1.3.0b1";
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <FlockDetection.h>  // Flock Safety / Raven / Penguin surveillance device detection
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
@@ -59,6 +60,11 @@ boolean is_5ghz = false;
 unsigned long side_b_reset_millis;
 unsigned long started_at_millis;
 unsigned long total_new_wifi = 0;
+
+// Flock Safety surveillance device detection
+FlockDetector flockDetector;
+unsigned int flock_detection_count = 0;
+File flock_json_file;
 
 uint32_t chip_id;
 
@@ -162,7 +168,7 @@ int web_timeout = 60000; //ms to spend hosting the web interface before booting.
 int gps_allow_stale_time = 60000;
 boolean enforce_valid_binary_checksums = true; //Lookup OTA binary checksums online, prevent installation if no match found
 boolean nets_over_uart = false; //Send discovered networks over UART?
-String ota_hostname = "ota.wardriver.uk";
+String ota_hostname = "JosephHewitt.github.io";
 unsigned long auto_reset_ms = 0;
 float force_lat = 0;
 float force_lon = 0;
@@ -180,25 +186,41 @@ boolean use_fallback_cert = false;
 
 static const char *PRIMARY_OTA_CERT = R"EOF(
 -----BEGIN CERTIFICATE-----
-MIIDeTCCAmGgAwIBAgIUbrbwBJuNsr7DeScXZxaUynePHfowDQYJKoZIhvcNAQEL
-BQAwTDELMAkGA1UEBhMCTkwxCzAJBgNVBAgMAlpIMRUwEwYDVQQKDAx3YXJkcml2
-ZXIudWsxGTAXBgNVBAMMEG90YS53YXJkcml2ZXIudWswHhcNMjMwNjA4MjAwMTQ1
-WhcNMjUwMTI4MjAwMTQ1WjBMMQswCQYDVQQGEwJOTDELMAkGA1UECAwCWkgxFTAT
-BgNVBAoMDHdhcmRyaXZlci51azEZMBcGA1UEAwwQb3RhLndhcmRyaXZlci51azCC
-ASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBANhPWzq8txiMt4IJikuZnNov
-6rAvAM3OicSKofdkOuvNOV6HlVmfzYVNNlESakuloEYRPwF7oxhQEPeU2X2jsQK6
-cCuWrAR2SWPTJ1kk+gNMx7Xq7GOU11wuHFJNRESdOCSCvixCjg/fbMb0Zmt9z/gX
-Rur0Pg/uYEcUgFyJ8KYgDh7m7chCfcFafhQ5RnkXpMINBZX+GmC/BQ57uZhrdTyY
-x5ZnjrLjzvjgLmABRTynCELPDjosfquxW+fHoG48qk4QLMhu/f8JItOce5kmIvS+
-v/766LN2gVK7oYlWjN44Sa/5hlp6Rl2YXGayYOAiivuyr/vniG0xoi2LBe1/WtsC
-AwEAAaNTMFEwHQYDVR0OBBYEFF5wxZNmrWN8/a2a0fAPmJJ/m+OaMB8GA1UdIwQY
-MBaAFF5wxZNmrWN8/a2a0fAPmJJ/m+OaMA8GA1UdEwEB/wQFMAMBAf8wDQYJKoZI
-hvcNAQELBQADggEBACpF2qkQd40MLWkMDYaoZFYeZMMt7ktsRAjo6P5HNVAQMdMz
-i9GtYLiXNFyw/Ub0X0JFwZDiqFSKcxJIWx5hgEVTSIvg36ZCRmrP1gmcVtzLbgjG
-oTlYBrUQdeH0KYG+7xMdPJI4+8yc3OXsoZjr4tIlbZJtej6OBipZks645BKUAs3a
-NUVm7tvzg9hEsfPDXXubcK6JLPdNwrnVEmwq6NlKVVHN9McExBumGKnyKYGK8MZF
-KwkScjhM4MVp5+qVrnuZgqkwM0ZOpZ/vAlD6Csv/DplY92nZs1vHSp2RDVHq6IFI
-IY8r4D96F4ocMmptiPuXifjDkGbXPqfnJhwhaMA=
+MIIGhTCCBW2gAwIBAgIRAJB3NEFHMW75lZl2eur98bkwDQYJKoZIhvcNAQELBQAw
+gY8xCzAJBgNVBAYTAkdCMRswGQYDVQQIExJHcmVhdGVyIE1hbmNoZXN0ZXIxEDAO
+BgNVBAcTB1NhbGZvcmQxGDAWBgNVBAoTD1NlY3RpZ28gTGltaXRlZDE3MDUGA1UE
+AxMuU2VjdGlnbyBSU0EgRG9tYWluIFZhbGlkYXRpb24gU2VjdXJlIFNlcnZlciBD
+QTAeFw0yNTAzMDcwMDAwMDBaFw0yNjAzMDcyMzU5NTlaMBYxFDASBgNVBAMMCyou
+Z2l0aHViLmlvMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxKQLElVm
+JYKnZ9dmKMWrb4fy4BWFm658EQemS4hJgrt+1NFpL2tGVaFupVyV3vmKorCX3zej
+c7+gH8Ugpemmj9r5tk1NZ0SXXALTjvT2i03oSqjwCzkn+R1o0TYg+G7PyQ5pE18A
+E+K3VUcpch1f5RyBTEvE4+HUg4/6OpAIYFVznJ3yk8a+bo1i/HBp2MbtPzssSlT8
+mPLY76SETtKdwgIdY91MkTiJd1x0KJDM2GPKM7pNTc81NMSw6WBzsxg4PFbR+BCY
+82/sYu8iMy/AdYcUz72hh2DGXnVypzzV/LLgJ/VAP5m+md0lVH5KIG/cduDrajlo
+LQ4LKJktO4VmwQIDAQABo4IDUjCCA04wHwYDVR0jBBgwFoAUjYxexFStiuF36Zv5
+mwXhuAGNYeEwHQYDVR0OBBYEFBLwftAxb+SvNbWJ+0LZ7bcLk80EMA4GA1UdDwEB
+/wQEAwIFoDAMBgNVHRMBAf8EAjAAMB0GA1UdJQQWMBQGCCsGAQUFBwMBBggrBgEF
+BQcDAjBJBgNVHSAEQjBAMDQGCysGAQQBsjEBAgIHMCUwIwYIKwYBBQUHAgEWF2h0
+dHBzOi8vc2VjdGlnby5jb20vQ1BTMAgGBmeBDAECATCBhAYIKwYBBQUHAQEEeDB2
+ME8GCCsGAQUFBzAChkNodHRwOi8vY3J0LnNlY3RpZ28uY29tL1NlY3RpZ29SU0FE
+b21haW5WYWxpZGF0aW9uU2VjdXJlU2VydmVyQ0EuY3J0MCMGCCsGAQUFBzABhhdo
+dHRwOi8vb2NzcC5zZWN0aWdvLmNvbTCCAX4GCisGAQQB1nkCBAIEggFuBIIBagFo
+AHYAlpdkv1VYl633Q4doNwhCd+nwOtX2pPM2bkakPw/KqcYAAAGVbeysdQAABAMA
+RzBFAiEA+YIgsAqb2cqQVlF4JP2ERIVCH3RXdB7DjIPc6Ch5aK4CIHjqUoV7F5Mk
+fcIQcmdn7Z5UR8nYtPA2OLvYc3mCFcLuAHcAGYbUxyiqb/66A294Kk0BkarOLXIx
+D67OXXBBLSVMx9QAAAGVbeysDgAABAMASDBGAiEAjryAbXlHsXj/v4f7CWXJzDUX
+SUuvA5kRH3doh4WPUQcCIQC+nojCqhCn/ZupbnI50O1T3FSKBQu/LOZ33fApzLJW
+hQB1AMs49xWJfIShRF9bwd37yW7ymlnNRwppBYWwyxTDFFjnAAABlW3srDcAAAQD
+AEYwRAIgS98L1D2W8nzV3tIQ0R4UJWxwxb7I/TT6e9ly0nA0QsACIFpl7s/WA1Qm
+z1Vm8ZtihoNFubO/AiiaVGaeDQiznHFCMHsGA1UdEQR0MHKCCyouZ2l0aHViLmlv
+ggwqLmdpdGh1Yi5jb22CFyouZ2l0aHVidXNlcmNvbnRlbnQuY29tggpnaXRodWIu
+Y29tgglnaXRodWIuaW+CFWdpdGh1YnVzZXJjb250ZW50LmNvbYIOd3d3LmdpdGh1
+Yi5jb20wDQYJKoZIhvcNAQELBQADggEBAHksjTVCptW9CtbBXu+7J2cDDmKRz/EA
+kUyONuojOnKoI3d2f5DQDkqzu/gSj6B28YO3a4EYFktvwq3KnXAu9KzSM1ehlhtA
+lxlvjjGUgXvux7DjnBH40ItKiE723opeWVbm2WExdRPSckm/CDwshz2U3Sl3M3Wt
+v0xPuZJrg1tMIL58RqrS5PpFlAIIlEUC6dr+xVQrwLNcYXVVgvZsRSX/YbrzboLM
+gWhuDSQPcaeDGHcy7NxRZHmlpHz+/Ot067VuxjGqm9veKNGZMUdroS+ocxAJBXv3
+Z1NCCowvpZazNxKccQg7izYwd6HL70WMxCWFU0e70uw9KZqteG7SVcQ=
 -----END CERTIFICATE-----
 )EOF";
 
@@ -2571,7 +2593,47 @@ void primary_scan_loop(void * parameter){
               continue;
             }
           }
-          
+
+          // ========== FLOCK SAFETY SURVEILLANCE DETECTION ==========
+          // Check if this device matches Flock Safety / Raven / Penguin patterns
+          GPSSnapshot gps_snapshot = get_gps_snapshot();
+          FlockDetection* detection = flockDetector.classify(
+            this_bssid,
+            ssid.c_str(),
+            WiFi.RSSI(i),
+            gps_snapshot,
+            WiFi.channel(i)
+          );
+
+          if (detection != nullptr) {
+            // SURVEILLANCE DEVICE DETECTED!
+            flock_detection_count++;
+
+            // Write to Flock-specific JSON log
+            if (!flock_json_file) {
+              String flock_filename = "/flock_detections_" + String(bootcount) + ".json";
+              flock_json_file = SD.open(flock_filename.c_str(), FILE_APPEND);
+            }
+
+            if (flock_json_file) {
+              String json_output = flockDetector.toJSON(detection);
+              flock_json_file.println(json_output);
+              flock_json_file.flush();
+            }
+
+            // Alert on LCD (if available)
+            Serial.println("!!! FLOCK DEVICE DETECTED !!!");
+            Serial.print("Type: ");
+            Serial.println(detection->device_type);
+            Serial.print("Threat Score: ");
+            Serial.println(detection->threat_score);
+            Serial.print("GPS: ");
+            Serial.print(detection->gps.latitude, 6);
+            Serial.print(", ");
+            Serial.println(detection->gps.longitude, 6);
+          }
+          // ========== END FLOCK DETECTION ==========
+
           filewriter.printf("%s,%s,%s,%s,%d,%d,%s,WIFI\n", this_bssid, ssid.c_str(), security_int_to_string(WiFi.encryptionType(i)).c_str(), dt_string(get_epoch()).c_str(), WiFi.channel(i), WiFi.RSSI(i), gps_string().c_str());
           if (nets_over_uart){
             Serial.printf("NET=%s,%s,%s,%s,%d,%d,%s,WIFI\n", this_bssid, ssid.c_str(), security_int_to_string(WiFi.encryptionType(i)).c_str(), dt_string(get_epoch()).c_str(), WiFi.channel(i), WiFi.RSSI(i), gps_string().c_str());
@@ -2924,6 +2986,83 @@ String parse_bside_line(String buff){
     }
   }
 
+  // ========== RAVEN GUNSHOT DETECTOR BLE DETECTION ==========
+  if (buff.indexOf("BL_RAVEN,") > -1) {
+    // Enhanced BLE format from Module B: BL_RAVEN,RSSI,MAC,NAME,UUIDs
+    int startpos = buff.indexOf("BL_RAVEN,")+9;
+    int endpos = buff.indexOf(",",startpos);
+    String rssi = buff.substring(startpos,endpos);
+
+    startpos = endpos+1;
+    endpos = buff.indexOf(",",startpos);
+    String mac_str = buff.substring(startpos,endpos);
+
+    startpos = endpos+1;
+    endpos = buff.indexOf(",",startpos);
+    String ble_name = buff.substring(startpos,endpos);
+
+    startpos = endpos+1;
+    String service_uuids = buff.substring(startpos);
+    service_uuids.trim();
+
+    unsigned char mac_bytes[6];
+    int values[6];
+
+    if (6 == sscanf(mac_str.c_str(), "%x:%x:%x:%x:%x:%x%*c", &values[0], &values[1], &values[2], &values[3], &values[4], &values[5])){
+      for(int i = 0; i < 6; ++i ){
+          mac_bytes[i] = (unsigned char) values[i];
+      }
+
+      if (!seen_mac(mac_bytes)){
+        save_mac(mac_bytes);
+
+        // RAVEN GUNSHOT DETECTOR DETECTED!
+        flock_detection_count++;
+        Serial.println("!!! RAVEN GUNSHOT DETECTOR (BLE) !!!");
+        Serial.print("MAC: ");
+        Serial.println(mac_str);
+        Serial.print("UUIDs: ");
+        Serial.println(service_uuids);
+
+        // Create FlockDetection record with GPS binding
+        GPSSnapshot gps_snapshot = get_gps_snapshot();
+        FlockDetection* detection = flockDetector.classifyBLE(
+          mac_str.c_str(),
+          ble_name.c_str(),
+          service_uuids.c_str(),
+          rssi.toInt(),
+          gps_snapshot
+        );
+
+        if (detection != nullptr) {
+          // Write to Flock-specific JSON log
+          if (!flock_json_file) {
+            String flock_filename = "/flock_detections_" + String(bootcount) + ".json";
+            flock_json_file = SD.open(flock_filename.c_str(), FILE_APPEND);
+          }
+
+          if (flock_json_file) {
+            String json_output = flockDetector.toJSON(detection);
+            flock_json_file.println(json_output);
+            flock_json_file.flush();
+          }
+
+          Serial.print("Threat Score: ");
+          Serial.println(detection->threat_score);
+          Serial.print("GPS: ");
+          Serial.print(detection->gps.latitude, 6);
+          Serial.print(", ");
+          Serial.println(detection->gps.longitude, 6);
+        }
+
+        // Also write to standard Wigle CSV for compatibility
+        mac_str.toUpperCase();
+        out = mac_str + "," + ble_name + ",[BLE-RAVEN]," + dt_string(get_epoch()) + ",0," + rssi + "," + gps_string() + ",BLE";
+      }
+    }
+  }
+  // ========== END RAVEN BLE DETECTION ==========
+
   if (buff.indexOf("RESET=") > -1) {
     if (millis() - started_at_millis < 10000){
       //We will ignore this if we recently started running because then it's normal behavior, not a fault.
@@ -3140,6 +3279,43 @@ String dt_string_from_gps(){
     datetime += nmea.getSecond();
   }
   return datetime;
+}
+
+/**
+ * Create GPSSnapshot from current NMEA GPS data
+ * Used for binding GPS coordinates to Flock detections
+ */
+GPSSnapshot get_gps_snapshot() {
+  GPSSnapshot gps;
+
+  // Get coordinates (NMEA returns microdegrees)
+  gps.latitude = (double)nmea.getLatitude() / 1000000.0;
+  gps.longitude = (double)nmea.getLongitude() / 1000000.0;
+
+  // Get altitude (NMEA returns millimeters)
+  long alt = 0;
+  if (nmea.getAltitude(alt)) {
+    gps.altitude = (float)alt / 1000.0;  // Convert to meters
+  } else {
+    gps.altitude = 0.0;
+  }
+
+  // Get HDOP (accuracy indicator, NMEA returns tenths)
+  gps.hdop = (float)nmea.getHDOP() / 10.0;
+
+  // Build UTC timestamp string
+  if (nmea.isValid()) {
+    snprintf(gps.timestamp, sizeof(gps.timestamp),
+             "%04d-%02d-%02d %02d:%02d:%02d",
+             nmea.getYear(), nmea.getMonth(), nmea.getDay(),
+             nmea.getHour(), nmea.getMinute(), nmea.getSecond());
+    gps.valid = (nmea.getHDOP() <= 250);  // HDOP <= 25.0 considered valid
+  } else {
+    strcpy(gps.timestamp, "NO_GPS_FIX");
+    gps.valid = false;
+  }
+
+  return gps;
 }
 
 String gps_string(){
